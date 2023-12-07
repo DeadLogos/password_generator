f557b77a71a54b123dfb2bc4922aeda9820d36db
DeadLogos <iwtfal11@gmail.com>
2023-12-07 19:27:32 +0300

    deleted config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-APP_TITLE = 'Password Generator'
-WINDOW_SIZE = '500x400'
-LOGO_FILENAME = 'logo.png'
-MAIN_COLOR = '#a2cc9d'
-FILL_COLOR = '#c2debf'
-FONT_STYLE = ('Palatino', 13)
-DUMP_FILENAME = 'passwords.txt'
-DESKTOP_DIR = 'C:/Users/iwtfa/OneDrive/Рабочий стол'
